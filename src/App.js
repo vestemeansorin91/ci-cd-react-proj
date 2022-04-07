025be72381b161aa49b06d304343377ef868b6b1
sorinvestemean <vestemeansorin@gmail.com>
2022-04-07 10:04:10 +0300

    code coverage + test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Now! 2022
         </a>
       </header>
     </div>
