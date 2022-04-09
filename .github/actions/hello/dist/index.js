9f00db3cb08db1720777ea1a0207444e9cc9a2ba
sorinvestemean <vestemeansorin@gmail.com>
2022-04-09 09:32:47 +0300

    feat: custom things

--- .github/actions/hello/dist/index.js
+++ .github/actions/hello/dist/index.js
@@ -881,14 +881,24 @@ const core = __webpack_require__(470);
 const github = __webpack_require__(469);
 
 try {
-   throw ( new Error ('some error message'));
+  // throw ( new Error ('some error message'));
+
+  // only if debug mode is enabled, just for logging does it not fail
+  core.debug('Debug message');
+  core.warning('Debug message');
+  core.error('Debug message');
+
   const name = core.getInput("who-to-greet");
+  core.setSecret(name);
   console.log(`Hello ${name}`);
 
   const time = new Date().toTimeString();
   core.setOutput("time", time);
 
+  core.startGroup('Logging github objecet');
   console.log(JSON.stringify(github, null, "\t"));
+  core.endGroup();
+  core.exportVariable('HELLO', 'Hello World from the variable');
 } catch (error) {
   core.setFailed(error.message);
 }
--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -2,14 +2,24 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 try {
-   throw ( new Error ('some error message'));
+  // throw ( new Error ('some error message'));
+
+  // only if debug mode is enabled, just for logging does it not fail
+  core.debug('Debug message');
+  core.warning('Debug message');
+  core.error('Debug message');
+
   const name = core.getInput("who-to-greet");
+  core.setSecret(name);
   console.log(`Hello ${name}`);
 
   const time = new Date().toTimeString();
   core.setOutput("time", time);
 
+  core.startGroup('Logging github objecet');
   console.log(JSON.stringify(github, null, "\t"));
+  core.endGroup();
+  core.exportVariable('HELLO', 'Hello World from the variable');
 } catch (error) {
   core.setFailed(error.message);
 }
