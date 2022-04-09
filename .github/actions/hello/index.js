95bfbd3e43017112b53859a9a293d1b1e0a9d109
sorinvestemean <vestemeansorin@gmail.com>
2022-04-09 10:17:14 +0300

    fix(updated github to getoctokit): fix

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -5,9 +5,9 @@ try {
   // throw ( new Error ('some error message'));
 
   // only if debug mode is enabled, just for logging does it not fail
-  core.debug('Debug message');
-  core.warning('Debug message');
-  core.error('Debug message');
+  core.debug("Debug message");
+  core.warning("Debug message");
+  core.error("Debug message");
 
   const name = core.getInput("who-to-greet");
   core.setSecret(name);
@@ -16,10 +16,10 @@ try {
   const time = new Date().toTimeString();
   core.setOutput("time", time);
 
-  core.startGroup('Logging github objecet');
+  core.startGroup("Logging github objecet");
   console.log(JSON.stringify(github, null, "\t"));
   core.endGroup();
-  core.exportVariable('HELLO', 'Hello World from the variable');
+  core.exportVariable("HELLO", "Hello World from the variable");
 } catch (error) {
   core.setFailed(error.message);
 }
